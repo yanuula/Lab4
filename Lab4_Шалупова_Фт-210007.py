6fb2a8d35c23796ab1cf9ac2c588901ad20c177a
yanuula <113615651+yanuula@users.noreply.github.com>
2022-10-21 01:36:28 +0500

    Update Lab4_Шалупова_Фт-210007.py

--- Lab4_Шалупова_Фт-210007.py
+++ Lab4_Шалупова_Фт-210007.py
@@ -1,7 +1,7 @@
-import rub
+import rub #Импортирвоание кода для вывода суммы в рублях, которую необходимо заплатить за просчитанный вариант (цифрами)
 while True:
     try:
-        employees_num = int(input('Введите число сотрудников: '))
+        employees_num = int(input('Введите число сотрудников: ')) #Ввод числа сотрудников
         assert 1 <= employees_num <= 1000
     except AssertionError:
         print('Число не входит в диапазон')
@@ -13,8 +13,8 @@ while True:
     distance_list = []
     price_list = []
     try:
-        distance_list = list(map(int, input("Введите расстояние в километрах, используя пробел: ").split()))
-        price_list = list(map(int, input("Введите тарифы за проезд одного километра, используя пробел: ").split())) 
+        distance_list = list(map(int, input("Введите расстояние в километрах, используя пробел: ").split())) #Ввод расстояния
+        price_list = list(map(int, input("Введите тарифы за проезд одного километра, используя пробел: ").split())) #Ввод тарифов
     except ValueError:
         print('Неверный ввод')
         break
@@ -22,7 +22,7 @@ while True:
     c_price_list = price_list[:]
     index_list_d = [] #Списки с индексами значений
     index_list_p = []
-    #Заполняем списки индексами
+    #Заполнение списков индексами
     max_distance = 0
     for i in range(employees_num):                              
         index = c_distance_list.index(max(c_distance_list))
@@ -32,13 +32,12 @@ while True:
         index = c_price_list.index(min(c_price_list))
         index_list_p.append(index)
         c_price_list[index] = 10**10
-    print('Такси для клиентов по порядку: ')
+    print('Такси для клиентов по порядку: ') #Вывод такси для клиентов
     for i in range(employees_num):
         taxi_num = index_list_p[index_list_d.index(i)]
         print(taxi_num+1)
     for i in range(employees_num):
         sum += distance_list[index_list_d[i]]*price_list[index_list_p[i]]
     print('Сумма: ')
-    print(sum)
-    rub.out(sum)
-
+    print(sum) #Вывод суммы
+    rub.out(sum) 
